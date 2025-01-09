5a3c51e6e56a7498db72d45b1dd4fcd124ff1c00
Milinda Karunaratne <milindakarunaratna55@gmail.com>
2025-01-09 23:40:25 +0530

    setup base project folders

---
+++ client-app/src/main/java/com/skydesk/client/ClientControlAppInitializer.java
@@ -0,0 +1,23 @@
+package com.skydesk.client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ClientControlAppInitializer extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primarystage) throws Exception {
+
+        Scene mainScene = new Scene(FXMLLoader.load(getClass().getResource("/scene/MainControlView.fxml")));
+        primarystage.setScene(mainScene);
+        primarystage.setTitle("Control Desktop");
+        primarystage.setResizable(true);
+        primarystage.show();
+        primarystage.centerOnScreen();
+    }
+}
