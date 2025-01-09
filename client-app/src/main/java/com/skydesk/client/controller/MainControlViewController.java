0169a286b4aa0d928e11fd38bf300761333a0347
Milinda Karunaratne <milindakarunaratna55@gmail.com>
2025-01-10 00:42:45 +0530

    Add logic for mouse move & key press

--- client-app/src/main/java/com/skydesk/client/controller/MainControlViewController.java
+++ client-app/src/main/java/com/skydesk/client/controller/MainControlViewController.java
@@ -1,9 +1,69 @@
 package com.skydesk.client.controller;
 
+import javafx.concurrent.Task;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+
+import java.awt.*;
+import java.io.*;
+import java.net.Socket;
+import java.security.Key;
 
 public class MainControlViewController {
     public AnchorPane root;
     public ImageView imageScene;
+    private Socket socket;
+
+    public void initialize() throws Exception {
+        imageScene.fitWidthProperty().bind(root.widthProperty());
+        imageScene.fitHeightProperty().bind(root.heightProperty());
+
+        socket = new Socket("192.168.179.79", 9090);
+
+        OutputStream osCoords = socket.getOutputStream();
+        BufferedOutputStream bosCoords = new BufferedOutputStream(osCoords);
+        ObjectOutputStream oosCoords = new ObjectOutputStream(bosCoords);
+
+        OutputStream osKeys = socket.getOutputStream();
+        BufferedOutputStream bosKeys = new BufferedOutputStream(osKeys);
+        ObjectOutputStream oosKeys = new ObjectOutputStream(bosKeys);
+
+        imageScene.setOnMouseMoved(mouseEvent -> {
+            try {
+                oosCoords.writeObject(new Point((int) mouseEvent.getX(), (int) mouseEvent.getY()));
+                oosCoords.flush();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        imageScene.setOnKeyPressed(keyEvent -> {
+            try {
+                oosKeys.writeObject(new Key[Integer.parseInt(keyEvent.getCharacter())]);
+                oosKeys.flush();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        Task<javafx.scene.image.Image> task = new Task<>() {
+            @Override
+            protected javafx.scene.image.Image call() throws Exception {
+                InputStream is = socket.getInputStream();
+                BufferedInputStream bis = new BufferedInputStream(is);
+                ObjectInputStream ois = new ObjectInputStream(bis);
+                while (true) {
+                    byte[] image = (byte[]) ois.readObject();
+                    ByteArrayInputStream bais = new ByteArrayInputStream(image);
+                    javafx.scene.image.Image screen = new Image(bais);
+                    updateValue(screen);
+                }
+            }
+        };
+
+        imageScene.imageProperty().bind(task.valueProperty());
+        new Thread(task).start();
+    }
+
 }
--- server-app/src/main/java/com/skydesk/server/ServerControlAppInitializer.java
+++ server-app/src/main/java/com/skydesk/server/ServerControlAppInitializer.java
@@ -1,4 +1,84 @@
 package com.skydesk.server;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.security.Key;
+
 public class ServerControlAppInitializer {
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSocket = new ServerSocket(9090);
+        System.out.println("Server started");
+        while (true) {
+            System.out.println("Waiting for connection");
+            Socket localSocket = serverSocket.accept();
+            System.out.println("Client connected from " + localSocket.getRemoteSocketAddress());
+
+            new Thread(() -> {
+                try {
+                    OutputStream os = localSocket.getOutputStream();
+//                    System.out.println(os.toString());
+                    BufferedOutputStream bos = new BufferedOutputStream(os);
+                    ObjectOutputStream oos = new ObjectOutputStream(bos);
+
+                    while (true) {
+                        Robot robot = new Robot();
+                        BufferedImage screen =  robot.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                        ImageIO.write(screen, "jpeg", baos);
+                        oos.writeObject(baos.toByteArray());
+                        oos.flush();
+                        Thread.sleep(1000/27);
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }).start();
+
+            new Thread(() -> {
+                try {
+                    InputStream isCoords = localSocket.getInputStream();
+                    BufferedInputStream bisCoords = new BufferedInputStream(isCoords);
+                    ObjectInputStream oisCoords = new ObjectInputStream(bisCoords);
+
+                    Robot robot = new Robot();
+                    while (true) {
+                        Point coordinates = (Point) oisCoords.readObject();
+                        robot.mouseMove(coordinates.x, coordinates.y);
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }).start();
+
+            new Thread(() -> {
+
+                try {
+                    InputStream isKeys = localSocket.getInputStream();
+                    BufferedInputStream bisKeys = new BufferedInputStream(isKeys);
+                    ObjectInputStream oisKeys = new ObjectInputStream(bisKeys);
+
+                    Robot robot = new Robot();
+
+                    while (true) {
+                        Key keys = (Key) oisKeys.readObject();
+                        robot.keyPress(keys.hashCode());
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }).start();
+        }
+
+
+    }
 }
