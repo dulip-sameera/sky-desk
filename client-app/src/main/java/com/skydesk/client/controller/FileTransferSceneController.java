fd8e4682a64a919c3bcdaffdc69c0c1c08d70f23
Dulip Sameera <dsgitrepo101@gmail.com>
2025-01-12 12:23:01 +0530

    Add module-info to shared

--- client-app/src/main/java/com/skydesk/client/controller/FileTransferSceneController.java
+++ client-app/src/main/java/com/skydesk/client/controller/FileTransferSceneController.java
@@ -1,11 +1,15 @@
 package com.skydesk.client.controller;
 
+import com.skydesk.shared.protocol.FileShareProtocol;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TreeView;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class FileTransferSceneController {
     public AnchorPane root;
@@ -21,6 +25,19 @@ public class FileTransferSceneController {
     }
 
     public void btnSelectFileOnAction(ActionEvent actionEvent) {
+        File initialDirectory = new File(System.getProperty("user.home"));
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open File");
+        fileChooser.setInitialDirectory(initialDirectory);
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("All Files", "*.*"));
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PDF Files", "*.pdf"));
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"));
+        File file = fileChooser.showOpenDialog(root.getScene().getWindow());
+        if (file == null) {return;}
+        System.out.println(file.getName());
+        System.out.println(file.length());
+        FileShareProtocol fileShareProtocol = new FileShareProtocol();
     }
 
     public void btnUploadOnAction(ActionEvent actionEvent) {
