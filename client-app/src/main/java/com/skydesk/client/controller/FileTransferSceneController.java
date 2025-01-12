8059ec8b118636da697502568b1924feafe5297e
Dulip Sameera <dsgitrepo101@gmail.com>
2025-01-12 11:14:29 +0530

    Update FileTransferSceneController

--- client-app/src/main/java/com/skydesk/client/controller/FileTransferSceneController.java
+++ client-app/src/main/java/com/skydesk/client/controller/FileTransferSceneController.java
@@ -1,4 +1,28 @@
 package com.skydesk.client.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TreeView;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
 public class FileTransferSceneController {
+    public AnchorPane root;
+    public Label lblRemote;
+    public TreeView trRemoteFileSystem;
+    public Button btnDownload;
+    public AnchorPane pnDropTarget;
+    public ImageView imgDrop;
+    public Button btnSelectFile;
+    public Button btnUpload;
+
+    public void btnDownloadOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSelectFileOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUploadOnAction(ActionEvent actionEvent) {
+    }
 }
