989a31afeaae3177da0826e50059c2a1bdd8b594
Dulip Sameera <dsgitrepo101@gmail.com>
2025-01-13 05:43:58 +0530

    Add Drag Events

--- client-app/src/main/java/com/skydesk/client/controller/FileTransferSceneController.java
+++ client-app/src/main/java/com/skydesk/client/controller/FileTransferSceneController.java
@@ -1,10 +1,13 @@
 package com.skydesk.client.controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TreeView;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 
@@ -20,8 +23,7 @@ public class FileTransferSceneController {
     public Button btnSelectFile;
     public Button btnUpload;
 
-    public void btnDownloadOnAction(ActionEvent actionEvent) {
-    }
+    private File selectedFile;
 
     public void btnSelectFileOnAction(ActionEvent actionEvent) {
         File initialDirectory = new File(System.getProperty("user.home"));
@@ -34,8 +36,47 @@ public class FileTransferSceneController {
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"));
         File file = fileChooser.showOpenDialog(root.getScene().getWindow());
 
+        // no file selected
+        if (file == null) return;
+
+        // check for permissions
+        if (!file.canRead()) {
+            alertPermissionDenied();
+            return;
+        }
+        selectedFile = file;
+
     }
 
     public void btnUploadOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDownloadOnAction(ActionEvent actionEvent) {
+    }
+
+    private void alertPermissionDenied() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Permission Denied");
+        alert.setHeaderText("Permission Denied");
+        alert.setContentText("You do not have permission to upload the file.");
+        alert.showAndWait();
+    }
+
+    public void pnDropTargetOnDragOver(DragEvent dragEvent) {
+        System.out.println("Drag over");
+
+        dragEvent.acceptTransferModes(TransferMode.COPY);
+    }
+
+    public void pnDropTargetOnDragEntered(DragEvent dragEvent) {
+        System.out.println("Drag entered");
+    }
+
+    public void pnDropTargetOnDragExited(DragEvent dragEvent) {
+        System.out.println("Drag exited");
+    }
+
+    public void pnDropTargetOnDragDropped(DragEvent dragEvent) {
+        System.out.println("Drag dropped");
     }
 }
